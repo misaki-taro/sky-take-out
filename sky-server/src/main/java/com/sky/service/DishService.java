c0225fc9d533a164c6f608f48deb62708501a6bb
misaki_acer <597234159@qq.com>
2023-09-12 15:06:39 +0800

    删除菜品业务代码开发

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,8 +1,21 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface DishService {
     public void saveWithFlavor(DishDTO dishDTO);
 
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    /**
+     * 菜品批量删除
+     *
+     * @param ids
+     */
+    void deleteBatch(List<Long> ids);
 }
